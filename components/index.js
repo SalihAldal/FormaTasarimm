3ca194ea781c63e27e060ecc255c5719cb18450b
SalihAldal <142041082+SalihAldal@users.noreply.github.com>
2023-08-17 13:59:24 +0300

    Add files via upload

---
+++ components/index.js
@@ -0,0 +1,14 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker.jsx";
+import ColorPicker from "./ColorPicker.jsx";
+import FilePicker from "./FilePicker.jsx";
+import Tab from "./Tab.jsx";
+
+
+export  {
+    CustomButton,
+    AIPicker,
+    ColorPicker,
+    FilePicker,
+    Tab,
+};
